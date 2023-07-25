2a801f9bcd9b0f8e1af649ce3001d3bcae9998a4
Konstantin <forjob378974@gmail.com>
2023-07-25 21:28:55 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Hello world " + Boolean.toString(bean == bean2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Cat " + Boolean.toString(cat1 == cat2));
     }
 }
